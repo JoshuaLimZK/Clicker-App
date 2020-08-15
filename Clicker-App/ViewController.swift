d217dde0a64d314175a049db6c15d56dda73976b
joshualimzk <joshua_lim_zhe_kai@s2019.ssts.edu.sg>
2020-08-15 22:34:24 +0800

    Initial Commit

---
+++ Clicker-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clicker-App
+//
+//  Created by Joshua Lim on 15/8/20.
+//  Copyright © 2020 Joshua Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
